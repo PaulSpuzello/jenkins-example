43b553dbacfcc2750a7d9cf6add0950d6e96142d
PaulSpuzello <paspuzello@dmacc.edu>
2020-05-28 16:03:39 -0500

    Initial Commit

---
+++ jenkin-example/src/test/java/FizzBuzzTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+class FizzBuzzTest {
+	
+	public FizzBuzz fB;
+	
+	@Test
+	public void testFizz() {
+		String fizzBuzz = fB.play(3);
+		Assertions.assertEquals(fizzBuzz, "Fizz");
+	}
+}
